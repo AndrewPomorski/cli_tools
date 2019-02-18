292ff0188aa3ccf082e148a0c6cf525dfeda1466
Andrew Pomorski <pomorski.andrew@gmail.com>
2019-02-18 14:15:25 +0800

    cleaned up some old stuffs

--- python/filecompare.py
+++ python/filecompare.py
@@ -42,7 +42,7 @@ with open(sys.argv[1]) as File1:
     content_1 = File1.readlines()
 
 content_1 = [x.strip() for x in content_1]
-print content_1
+#print content_1
 
 
 
@@ -50,9 +50,9 @@ with open(sys.argv[2]) as File2:
     content_2 = File2.readlines()
 
 content_2 = [x.strip() for x in content_2]
-print content_2
+#print content_2
 
 if content_1 == content_2:
     print("\n Compare: Files are the same.\n")
 else:
-    print("\n Files are not the same.\n")
+    print("\n Compare: Files are not the same.\n")
